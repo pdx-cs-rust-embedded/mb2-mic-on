4be78f22e8ee6ba8c1b1e37e1a13fed13cabe4ff
Bart Massey <bart.massey@gmail.com>
2024-01-22 22:55:36 -0800

    added board

---
+++ src/bin/mic-on-board.rs
@@ -0,0 +1,23 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use microbit::{
+    board::Board,
+    hal::gpio,
+};
+
+#[entry]
+fn main() -> ! {
+    let board = Board::take().unwrap();
+    // XXX No high-drive.
+    let _mic_run = board.microphone_pins.mic_run.into_push_pull_output(gpio::Level::High);
+
+    // Loop forever.
+    loop {}
+}
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo<'_>) -> ! {
+    loop {}
+}
