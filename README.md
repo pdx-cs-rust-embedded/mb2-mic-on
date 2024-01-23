44ec056f84836bd409928ce08f04b10f11fb2a99
Bart Massey <bart.massey@gmail.com>
2024-01-22 21:48:35 -0800

    reached first working version

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# mb2-template: MicroBit 2 Rust embedded project template
+# mb2-mic-on: low…high level Rust pin-setting
+Bart Massey 2024
+
+This code turns on the MicroBit v2 microphone, at varying
+levels of abstraction:
+
+* `mic-on-raw`: Uses arithmetic and the manual.
+* `mic-on-structured`: Hooray, subroutines.
+* `mic-on-pac`: Uses `nrf52833-pac`.
+* `mic-on-hal`: Uses `nrf52833-hal`.
+* `mic-on-board`: Uses `microbit-v2`.
+* `mic-on-embassy`: Uses Embassy `microbit-bsp`.
+
+# Acknowledgments
+
+The excellent [Comprehensive
+Rust](https://google.github.io/comprehensive-rust/bare-metal/microcontrollers/mmio.html)
+contains some very similar code.
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 #![no_main]
 #![no_std]
 
-use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
-
 use cortex_m_rt::entry;
-use microbit::board::Board;
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-    let _board = Board::take().unwrap();
-    let mut counter = 0u64;
-    loop {
-        rprintln!("{}", counter);
-        counter += 1;
+    // Write 1 to MIC_POWER pin.
+    unsafe {
+        // GPIO P0 base address.
+        let base = 0x50000000_usize;
+        // GPIO P0 configuration register.
+        let cr = (base + 0x700 + 4 * 20) as *mut u32;
+        let mut config = 0;
+        // A: Output pin.
+        config |= 1 << 0;
+        // B: Disable input buffer.
+        config |= 1 << 1;
+        // C: Disable pull-up and pull-down resistors.
+        config |= 0 << 2;
+        // D: Standard-drive 0, high-drive 1.
+        config |= 2 << 8;
+        // E: Pin sense.
+        config |= 0 << 16;
+        cr.write_volatile(config);
+        // GPIO P0 ouput bit-setting register.
+        let out_set = (base + 0x508) as *mut u32;
+        // Set P0.19.
+        out_set.write_volatile(1 << 20);
     }
+
+    // Loop forever.
+    loop {}
+}
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo<'_>) -> ! {
+    loop {}
 }
